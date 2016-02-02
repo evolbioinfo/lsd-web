e60b367cf63f68c0bea40f0e46e3a50bcd362419
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-02-02 12:09:08 +0100

    Try to include static files...  Does not work yet

--- lsd_web/lsd/tasks.py
+++ lsd_web/lsd/tasks.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+import time
 
 @shared_task
 def test(param):
+    time.sleep(30)
     return 'The test task (id=%s) executed with argument "%s" ' % (test.request.id,param)
-
--- lsd_web/lsd_web/settings.py
+++ lsd_web/lsd_web/settings.py
@@ -126,4 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(os.path.abspath(
+    os.path.join(BASE_DIR, 'lsd_web', 'static/')), '/')
+
 STATIC_URL = '/static/'
